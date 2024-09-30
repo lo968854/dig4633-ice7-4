fc02822078f3a777f766f29ab5e77f48b96088f4
ithomas1995 <78786623+ithomas1995@users.noreply.github.com>
2024-09-30 17:18:19 -0400

    BookCandlePuzzle
     Place books into specific sockets and then candle turns on

---
+++ Assets/SocketManager.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+public class SocketManager : MonoBehaviour
+{
+    // Event that gets invoked when the socket is occupied
+    [System.Serializable]
+    public class SocketOccupiedEvent : UnityEvent<GameObject> { }
+
+    public SocketOccupiedEvent onSocketOccupied;
+
+    // This will store the current object occupying the socket
+    private GameObject currentOccupant;
+
+    // Method to occupy the socket
+    public void OccupySocket(GameObject occupant)
+    {
+        if (currentOccupant == null)
+        {
+            currentOccupant = occupant;
+            Debug.Log($"{occupant.name} has occupied the socket.");
+            onSocketOccupied.Invoke(occupant);
+        }
+        else
+        {
+            Debug.LogWarning("Socket is already occupied!");
+        }
+    }
+
+    // Method to release the socket
+    public void ReleaseSocket()
+    {
+        if (currentOccupant != null)
+        {
+            Debug.Log($"{currentOccupant.name} has released the socket.");
+            currentOccupant = null;
+        }
+        else
+        {
+            Debug.LogWarning("Socket is already empty!");
+        }
+    }
+}
